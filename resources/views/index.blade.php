7329c500990074e3c87ac9d8162123ec7cce67ab
Максим Куранов <alphthemech@gmail.com>
2022-03-22 17:52:09 +0400

    init

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -127,7 +127,7 @@
     <div class="container"> <h1>@lang('main.all_products')</h1>
         <form  method="GET" action="{{route("index")}}">
             <div class="filters row">
-                <div class="col-sm-6 col-md-3">
+                <div class="col-xl-12 col-sm-6 col-md-3 mt-2">
                     <label for="price_from">@lang('main.price_from')
                         <input type="text" name="price_from" id="price_from" size="6" value="{{ request()->price_from}}">
                     </label>
@@ -135,22 +135,23 @@
                         <input type="text" name="price_to" id="price_to" size="6"  value="{{ request()->price_to }}">
                     </label>
                 </div>
-                <div class="col-sm-2 col-md-2">
+                
+                <div class="col-sm-2 col-md-2 mt-2">
                     <label for="hit">
                         <input type="checkbox" name="hit" id="hit" @if(request()->has('hit')) checked @endif> @lang('main.properties.hit')
                     </label>
                 </div>
-                <div class="col-sm-2 col-md-2">
+                <div class="col-sm-2 col-md-2 mt-2">
                     <label for="new">
                         <input type="checkbox" name="new" id="new" @if(request()->has('new')) checked @endif> @lang('main.properties.new')
                     </label>
                 </div>
-                <div class="col-sm-2 col-md-2">
+                <div class="col-sm-2 col-md-2 mt-2">
                     <label for="recommend">
                         <input type="checkbox" name="recommend" id="recommend" @if(request()->has('recommend')) checked @endif> @lang('main.properties.recommend')
                     </label>
                 </div>
-                <div class="col-sm-6 col-md-3">
+                <div class="col-xl-12 col-sm-6 col-md-3 mt-2">
                     <button type="submit" class="btn btn-primary">@lang('main.filter')</button>
                     <a href="{{ route("index") }}" class="btn btn-warning">@lang('main.reset')</a>
                 </div>
